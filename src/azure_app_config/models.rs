468aa1a39068bad7337ff4ee339a688e7f0c886d
Mason Adams <mason.adams@msn.com>
2024-05-03 14:11:28 -0700

    functional

--- src/azure_app_config/models.rs
+++ src/azure_app_config/models.rs
@@ -2,28 +2,65 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct KeyValueSet{
-    items: Vec<KeyValue>
+    pub items: Vec<KeyValue>
 }
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct KeyValue{
-    etag: String,
-    key: String,
-    label: Option<String>,
-    content_type: Option<String>,
-    value: String,
-    tags: std::collections::HashMap<String, String>,
-    locked: bool,
-    last_modified: String
+    pub etag: String,
+    pub key: String,
+    pub label: Option<String>,
+    pub content_type: Option<String>,
+    pub value: String,
+    pub tags: std::collections::HashMap<String, String>,
+    pub locked: bool,
+    pub last_modified: String
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct KeySet{
-    items: Vec<Key>
+    pub items: Vec<Key>
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Key{
-    name: String
+    pub name: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FeatureFlagSet{
+    pub items: Vec<FeatureFlag>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FeatureFlag{
+    pub id: String,
+    pub enabled: bool
+}
+
+impl FeatureFlagSet{
+    pub fn from_key_value_set(kv_set: KeyValueSet) -> Self{
+        let mut flags = Vec::new();
+        for kv in kv_set.items{
+            flags.push(FeatureFlag::from_key_value(kv));
+        }
+        Self{
+            items: flags
+        }
+    }
+    
+    pub fn as_hash_map(&self) -> std::collections::HashMap<String, bool>{
+        let mut map = std::collections::HashMap::new();
+        for flag in &self.items{
+            map.insert(flag.id.clone(), flag.enabled);
+        }
+        map
+    }
+}
+
+impl FeatureFlag{
+    pub fn from_key_value(kv: KeyValue) -> Self{
+        serde_json::from_str(kv.value.as_str()).unwrap()
+    }
 }
 
 impl KeyValueSet {
